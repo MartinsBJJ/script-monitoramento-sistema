6c3b1f5cc308a91d80a28aa753b78b64d5297ee6
Martinsjj <lucasmartknd@gmail.com>
2025-01-20 21:30:42 -0300

    Alterei o nome do sistema

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_DIR="monitoramento_sistema"
+LOG_DIR="monitoramento_server"
 mkdir -p $LOG_DIR
 
 grep -E "(fail(ed)?|error|denied|unauthorized)" /var/log/syslog | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoramento_logs_sistema.txt

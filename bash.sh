18ec520528f3bebe15c4c8161610b4343e6c1c6e
Gustavo <gustavosantoscortez@gmail.com>
2025-01-21 11:59:28 -0300

    alterei o nome do diretório

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_DIR="monitoramento_server"
+LOG_DIR="monitoramento_system"
 mkdir -p $LOG_DIR
 
 grep -E "(fail(ed)?|error|denied|unauthorized)" /var/log/syslog | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoramento_logs_sistema.txt
